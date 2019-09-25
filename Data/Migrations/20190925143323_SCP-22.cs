cfb20ba3bddf4486a9882ba91f863a2b268b239f
Miles Wells <miles@svtrobotics.com>
2019-09-25 10:41:52 -0400

    SCP-22 added sproc to migration

--- Data/Migrations/20190925143323_SCP-22.cs
+++ Data/Migrations/20190925143323_SCP-22.cs
@@ -379,6 +379,128 @@ namespace SVT.Platform.Data.Migrations
                 name: "IX_Locations_LocationType",
                 table: "Locations",
                 column: "LocationType");
+
+            migrationBuilder.Sql(@"
+                CREATE OR ALTER PROCEDURE [dbo].[usp_cart_move]
+                    @AreaId             INT             = NULL
+                    , @CartId           NVARCHAR(50)	= NULL
+                    , @DeliveryId		INT				= -1
+                    , @DeliveryType		NVARCHAR(50)    = NULL
+                    , @DestinationId    INT				= -1
+                    , @OrderId			NVARCHAR(50)	= NULL
+                    , @Reserved			BIT				= 0
+                    , @SourceId         INT				= -1
+                    , @User             NVARCHAR(256)	= NULL
+                    , @NewDeliveryId    INT				= -1 OUTPUT
+                AS
+                BEGIN
+                    SET XACT_ABORT, NOCOUNT ON;
+
+                    DECLARE
+                        @OutputTable TABLE(NewDeliveryId INT);
+                    
+                    DECLARE
+                        @Completed		DATETIME2 = NULL
+                        , @Delivery		INT
+                        , @TranCount	INT;
+
+                    BEGIN TRY
+                        SELECT @TranCount = @@TRANCOUNT;
+
+                        IF @TranCount = 0 BEGIN TRANSACTION;
+
+                        IF (@DestinationId = -1 AND @SourceId = -1)
+                            THROW 55555, N'A Valid @Destination and/or @SourceId Required', 1;
+
+                        IF @User IS NULL
+                            SET @User = SYSTEM_USER;
+
+                        IF (@DestinationId <> -1 AND @Reserved = 0)
+                            SET @Completed = GETDATE();
+
+                        SET @Delivery = @DeliveryId;
+
+                        IF @Delivery = -1
+                        BEGIN
+                            IF (@AreaId IS NULL OR @CartId IS NULL OR @DeliveryType IS NULL OR @User IS NULL)
+                                THROW 55555, N'A Valid CartId Required When Creating a New Delivery', 1;
+
+                            INSERT INTO dbo.Deliveries
+                                (CartId, OrderId, Completed, UserId, DeliveryType, DestinationAreaId)
+                            OUTPUT
+                                INSERTED.DeliveryId
+                            INTO
+                                @OutputTable
+                            VALUES
+                                (@CartId, @OrderId, @Completed, @User, @DeliveryType, @AreaId);
+
+                            IF @@ROWCOUNT <> 1
+                                THROW 55555, N'Failed to Create Delivery', 1;
+
+                            SELECT @Delivery = NewDeliveryId FROM @OutputTable;
+
+                            SET @NewDeliveryId = @Delivery;
+                        END
+                        ELSE
+                        BEGIN
+                            IF @Completed IS NOT NULL
+                            BEGIN
+                                UPDATE dbo.Deliveries
+                                SET
+                                    Completed = @Completed
+                                    , ModifiedBy = @User
+                                    , ModifiedOn = GETDATE()
+                                WHERE
+                                    DeliveryId = @Delivery;
+
+                                IF @@ROWCOUNT <> 1
+                                    THROW 55555, N'Failed to Update Delivery Completion', 1;
+                            END
+                        END
+
+                        IF @SourceId <> -1
+                        BEGIN
+                            UPDATE dbo.Locations
+                            SET
+                                DeliveryId = NULL
+                                , ModifiedBy = @User
+                                , ModifiedOn = GETDATE()
+                                , Reserved = 0
+                            WHERE
+                                LocationId = @SourceId
+                                AND (DeliveryId = @Delivery OR DeliveryId IS NULL);
+
+                            IF @@ROWCOUNT <> 1
+                                THROW 55555, N'Failed to Update Source Cart Location', 1;
+                        END
+                        
+                        IF @DestinationId <> -1
+                        BEGIN
+
+                            UPDATE dbo.Locations
+                            SET
+                                DeliveryId = @Delivery
+                                , ModifiedBy = @User
+                                , ModifiedOn = GETDATE()
+                                , Reserved = @Reserved
+                            WHERE
+                                LocationId = @DestinationId
+                                AND (DeliveryId IS NULL OR DeliveryId = @Delivery);
+
+                            IF @@ROWCOUNT <> 1
+                                THROW 55555, N'Failed to Update Destination Cart Location', 1;
+                        END
+
+                        IF @TranCount = 0 COMMIT TRANSACTION;
+                    END TRY
+                    BEGIN CATCH
+                        IF XACT_STATE() <> 0 AND @TranCount = 0 
+                            ROLLBACK TRANSACTION;
+                        THROW;
+                    END CATCH
+                END
+                GO
+            ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
