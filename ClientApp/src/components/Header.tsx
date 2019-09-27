da058db3b75d12ac89f8c7592a58dd01e162ff4e
Miles Wells <miles@svtrobotics.com>
2019-09-27 09:53:40 -0400

    SCP-22 ugly nav WIP

--- ClientApp/src/components/Header.tsx
+++ ClientApp/src/components/Header.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row } from 'react-bootstrap';
 import { makeStyles } from '@material-ui/styles';
 
 import { ToolkitLogo } from 'icons';
+import Button from './Button';
+import { Drawer, List, ListItem } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
   flexContainerOverride: {
@@ -14,13 +17,36 @@ const useStyles = makeStyles({
 
 export default function Header() {
   const classes = useStyles();
+  const [navIsOpen, setNavIsOpen] = useState(false);
   return (
     <>
       <Row className={classes.flexContainerOverride}>
         <ToolkitLogo />
         {/* @missing-assets logo goes here */}
-        {/* @next Hamburger menu icon goes here */}
+        <Button variant='primary' onClick={() => setNavIsOpen(true)}>
+          Nav
+        </Button>
       </Row>
+      <Drawer
+        open={navIsOpen}
+        anchor='right'
+        onClose={() => setNavIsOpen(false)}
+      >
+        <List>
+          <ListItem button>
+            <Link to='/request/delivery'>Delivery Request</Link>
+          </ListItem>
+          <ListItem button>
+            <Link to='/request/cart'>Cart Handling</Link>
+          </ListItem>
+          <ListItem button>
+            <Link to='/request/staging'>Staging Management</Link>
+          </ListItem>
+          <ListItem button>
+            <Link to='/request/clean'>Clean Request</Link>
+          </ListItem>
+        </List>
+      </Drawer>
     </>
   );
 }
